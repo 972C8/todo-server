b6388fcc30d3db192efa20bfc73e0a3f90726080
tiborhaller <tiborhaller@gmail.com>
2021-06-08 15:48:04 +0200

    Add ScheduledExport to automatically export data for a specified interval

--- src/data/ReadWriteData.java
+++ src/data/ReadWriteData.java
@@ -2,10 +2,12 @@ package data;
 
 import com.google.gson.reflect.TypeToken;
 import server.Account;
+import server.Model;
 import server.TodoItem;
 import utility.BaseReadWrite;
 
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 /**
  * Reads and writes data from the given HashMap to a specified json file.
@@ -65,5 +67,43 @@ public class ReadWriteData<T> extends BaseReadWrite<HashMap<Integer, Account>> {
         //Set the id to the highest id + 1
         Account.setNextId(++accountId);
         TodoItem.setNextId(++todoId);
+    }
+
+    /**
+     * Inner class to schedule a data export for a specified interval.
+     * <p>
+     * Ensures that data (accounts + todos) is backed up frequently.
+     */
+    public static class ScheduledExport extends Thread {
+        Logger logger = Logger.getLogger("");
+
+        //The specified interval in minutes
+        int interval;
+
+        public ScheduledExport(int interval) {
+            this.interval = interval;
+        }
+
+        @Override
+        public void run() {
+            Model model = new Model();
+            //interval in ms
+            int msInterval = interval * 1000 * 60;
+
+            logger.info("Automatic data export is scheduled for every " + interval + " minutes");
+
+            while (true) {
+                try {
+                    //Sleep for configured interval
+                    Thread.sleep(msInterval);
+
+                    logger.info("Automatic data export started. (Every " + interval + " minutes)");
+                    //Write data to file
+                    model.writeData();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
